13d23958d8ec650c5d840785b64017dc1dbe77be
Jackline-ke <antoninahodhiambo@gmail.com>
2023-04-20 14:46:16 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  let [modifiedCounter, setModifiedCounter] = useState(1);
+
+
+  useEffect(() => {
+    console.log("useEffect Triggered");
+    setModifiedCounter(executeFizzBuzz)
+  }, [modifiedCounter])
+
+  const executeFizzBuzz = () => {
+
+    if (counter % 3 == 0 && counter % 5 == 0) {
+      setModifiedCounter = "FizzBuzz";
+    }
+    else if (counter % 5 == 0) {
+      setModifiedCounter = "Buzz";
+    }
+    else if (counter % 3 == 0) {
+      setModifiedCounter = "Fizz";
+    }
+    else {
+      setModifiedCounter = counter;
+    }
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
